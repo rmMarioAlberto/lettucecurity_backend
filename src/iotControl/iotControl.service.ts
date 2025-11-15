e340d9a5fb3d3a70db317b0a6b07f7503ab0e0df
rmMarioAlberto <rmmarioalberto08@gmail.com>
2025-11-14 21:27:47 -0600

    fix url railway

--- src/iotControl/iotControl.service.ts
+++ src/iotControl/iotControl.service.ts
@@ -63,7 +63,7 @@ export class IotControlService {
 
         let imageResult = 'Desconocido';
         try {
-          const response = await axios.post('https://web-production-02772.up.railway.app/', {
+          const response = await axios.post('https://web-production-02772.up.railway.app/predict', {
             image: imageUrl,
           });
           console.log(response);
