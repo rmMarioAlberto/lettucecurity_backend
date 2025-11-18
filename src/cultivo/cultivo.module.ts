161775e205b5ffe04f46510c00b30ad237e5095b
rmMarioAlberto <rmmarioalberto08@gmail.com>
2025-11-18 14:28:34 -0600

    cultivoModulo feature

--- src/cultivo/cultivo.module.ts
+++ src/cultivo/cultivo.module.ts
@@ -4,10 +4,11 @@ import { CultivoService } from "./cultivo.service";
 import { PrismaPostgresModule } from "../prisma/prismaPostgres.module";
 import { AuthModule } from "../auth/auth.module";
 import { TokensModule } from "../tokens/tokens.module";
+import { PrismaMongoModule } from "src/prisma/prismaMongo.module";
 
 @Module({
     controllers : [CultivoController],
     providers : [CultivoService],
-    imports : [PrismaPostgresModule,AuthModule,TokensModule]
+    imports : [PrismaPostgresModule,PrismaMongoModule,AuthModule,TokensModule]
 })
 export class ModuleCultivo {}
